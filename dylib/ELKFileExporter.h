ca9126e3aff93b2586a7c6844d253318474fb871
lpsManman2023 <134466969+lpsManman2023@users.noreply.github.com>
2026-07-17 03:28:37 +0800

    Clean up ELKFileExporter by removing methods
    Removed unused method declarations from ELKFileExporter.

--- dylib/ELKFileExporter.h
+++ dylib/ELKFileExporter.h
@@ -7,13 +7,8 @@
 
 @interface ELKFileExporter : NSObject
 
-/// 安装预览拦截 Hook（在 dylib 初始化时调用）
-+ (void)installPreviewHooks;
-
-/// 从消息对象导出文件
 + (void)exportFileFromMessage:(id)message;
-
-/// 显示提示框
++ (NSString *)findDecryptedFileInView:(UIView *)view;
 + (void)showAlertWithTitle:(NSString *)title message:(NSString *)message;
 
 @end
